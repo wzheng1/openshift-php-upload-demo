94ce11f9727ae090eb94d4f9295820aab52b4927
Wenjing Zheng <wzheng@redhat.com>
2016-01-05 15:20:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,4 +15,4 @@
  * @since         CakePHP(tm) v 0.10.0.1076
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-require 'webroot' . DIRECTORY_SEPARATOR . 'upload.php';
+echo "Hello World!";
